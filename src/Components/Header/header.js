23808427e03e392394a99969411de6dc6c34dd5a
Kanchanak <kanchanak@Kanchanaks-MacBook-Pro.local>
2021-06-01 19:11:42 -0700

    pushing some files for portfolio project to start with

---
+++ src/Components/Header/header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./header.css"
+import lotusHeader from './../../Images/lotus-header.jpg'
+import lotus1 from './../../Images/lotus1.png'
+import lotus2 from './../../Images/lotus2.png'
+
+
+export default function header() {
+    return (
+        <div className="header">
+            <div className="headerTitles">
+                <img className="headerImgLogo" src={lotus1} alt="Little lotus logo1"/>
+
+                <span className="headerTitleLg">Blog</span>
+                <img className="headerImgLogo" src={lotus2} alt="Little lotus logo2"/>
+
+            </div>
+            <img className="headerImg" src={lotusHeader} alt=""></img>
+        </div>
+    )
+}
---
+++ src/Components/Pages/blog/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from "../../Header/header"
+import './blog.css'
+
+export default function blog() {
+    return (
+        <div className="home">
+            <Header />
+            about
+        </div>
+    )
+}
